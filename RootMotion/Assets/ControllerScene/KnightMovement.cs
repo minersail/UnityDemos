a20087d036c6ce4d94832d968545afac5cca42d5
J Woo <jkwoo1234@gmail.com>
2018-06-14 19:44:56 -0400

    added controller test

---
+++ RootMotion/Assets/ControllerScene/KnightMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightMovement : MonoBehaviour
+{
+    Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();	
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetTrigger("TurnLeft");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("TurnRight");
+        }
+
+        anim.SetFloat("VelocityX", Input.GetAxis("Horizontal"));
+        anim.SetFloat("VelocityZ", Input.GetAxis("Vertical"));
+    }
+}
