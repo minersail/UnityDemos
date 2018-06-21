50474d00ce3d860788b80cbaed919544ad66ca79
minersail <jkwoo1234@gmail.com>
2018-06-21 15:21:40 -0400

    Set up AI

---
+++ AnimationTests/Assets/Character Locomotion/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health = 50;
+
+    private bool dead;
+    private float cooldown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldown += Time.deltaTime;
+	}
+
+    public void Damage(float amount)
+    {
+        if (dead || cooldown < 0.5) return;
+
+        health -= amount;
+        cooldown = 0;
+
+        if (health <= 0)
+        { 
+            GetComponent<Animator>().SetTrigger("Dying");
+            dead = true;
+
+            foreach (CapsuleCollider cc in GetComponentsInChildren<CapsuleCollider>())
+            {
+                cc.enabled = false;
+            }
+
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+}
---
+++ AnimationTests/Assets/Character Locomotion/Sword.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public float damage = 10;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != gameObject.layer)
+        {
+            if (other.GetComponentInParent<Animator>() != null)
+            {
+                Animator anim = other.GetComponentInParent<Animator>();
+                if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Hit") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Dying"))
+                {
+                    anim.SetTrigger("Hit");
+                }
+            }
+            
+            if (other.GetComponentInParent<Health>() != null)
+            {
+                if (other.GetComponentInParent<Animator>().GetBool("Blocking"))
+                {
+                    other.GetComponentInParent<Health>().Damage(damage / 4);
+                }
+                else
+                {
+                    other.GetComponentInParent<Health>().Damage(damage);
+                }
+            }
+        }
+    }
+}
