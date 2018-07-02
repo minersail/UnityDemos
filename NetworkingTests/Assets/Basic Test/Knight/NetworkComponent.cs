9c743c363bd6ef6b317d8da243c9450de31f6065
Jordan Woo <jkwoo1234@gmail.com>
2018-07-02 17:25:07 -0400

    d

---
+++ NetworkingTests/Assets/Basic Test/Knight/NetworkComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkComponent : Photon.PunBehaviour, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(GetComponentInChildren<Health>().health);
+        }
+        else
+        {
+            // Network player, receive data
+            GetComponentInChildren<Health>().health = (float)stream.ReceiveNext();
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		if (!photonView.isMine)
+        {
+            GetComponentInChildren<Camera>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*if (GetComponentInChildren<Health>().health <= 0f)
+        {
+            Manager.Instance.LeaveRoom();
+        }*/
+    }
+}
