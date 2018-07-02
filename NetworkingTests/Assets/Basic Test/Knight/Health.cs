63be3879a830b15b0e6061daefdea839b295266d
Jordan Woo <jkwoo1234@gmail.com>
2018-07-02 16:46:19 -0400

    added knight model

---
+++ NetworkingTests/Assets/Basic Test/Knight/Health.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 50;
+
+    [HideInInspector]
+    public float health;
+    [HideInInspector]
+    public bool dead;
+
+    private float cooldown;
+
+	// Use this for initialization
+	void Start () {
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldown += Time.deltaTime;
+	}
+
+    public void Damage(float amount)
+    {
+        if (dead || cooldown < 0.5) return;
+
+        health -= amount;
+        cooldown = 0;
+
+        if (health <= 0)
+        {
+            GetComponent<Animator>().SetTrigger("Dying");
+            GetComponent<Animator>().ResetTrigger("Hit"); // Make sure can't be hit during death animation
+            dead = true;
+
+            foreach (CapsuleCollider cc in GetComponentsInChildren<CapsuleCollider>())
+            {
+                cc.enabled = false;
+            }
+
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+}
---
+++ NetworkingTests/Assets/Basic Test/Knight/KnightMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightMovement : MonoBehaviour
+{
+    Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();	
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetTrigger("TurnLeft");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("TurnRight");
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("Attack");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("JumpAttack");
+        }
+
+        anim.SetBool("Blocking", Input.GetKey(KeyCode.LeftShift));        
+        anim.SetFloat("VelocityX", Input.GetAxis("Horizontal"));
+        anim.SetFloat("VelocityZ", Input.GetAxis("Vertical"));
+    }
+}
