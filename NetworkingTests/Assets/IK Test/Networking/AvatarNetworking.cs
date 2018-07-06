4e9aa15b08a5ca956ee31f0fde120d6b43b09b36
Jordan Woo <jkwoo1234@gmail.com>
2018-07-06 15:20:50 -0400

    different views for player and others

--- NetworkingTests/Assets/IK Test/Networking/AvatarNetworking.cs
+++ NetworkingTests/Assets/IK Test/Networking/AvatarNetworking.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AvatarNetworking : Photon.PunBehaviour, IPunObservable
 {
+    public GameObject leftHandMesh;
+    public GameObject rightHandMesh;
+    public GameObject avatarBody;
+    public GameObject avatarCamera;
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.isWriting)
@@ -19,5 +24,19 @@ public class AvatarNetworking : Photon.PunBehaviour, IPunObservable
     public void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void Start()
+    {
+        if (photonView.isMine)
+        {
+            avatarBody.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            leftHandMesh.GetComponent<MeshRenderer>().enabled = false;
+            rightHandMesh.GetComponent<MeshRenderer>().enabled = false;
+            avatarCamera.SetActive(false);
+        }
     }
 }
