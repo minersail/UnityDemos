7b79c98e7143e654ee1ff8483ec92b8a0423e9d7
minersail <jkwoo1234@gmail.com>
2018-08-07 23:07:56 -0400

    Added injury tests

---
+++ AnimationTests/Assets/Injured Motion/InjuryMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InjuryMovement : MonoBehaviour
+{
+    Animator anim;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        anim.SetFloat("Velocity", Mathf.Lerp(anim.GetFloat("Velocity"), Input.GetAxis("Vertical"), Time.deltaTime * 3));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            anim.SetBool("Left", !anim.GetBool("Left"));
+	}
+}
